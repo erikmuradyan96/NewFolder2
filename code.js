29a26d3cf4fb49c8d8c100960150b62d181f6ab1
Erik <erik.muradyan.96@gmail.com>
2023-10-12 20:25:48 +0400

    add new code

---
+++ code.js
@@ -0,0 +1,7 @@
+let image = document.querySelector(".image")
+image.style.width="500px"
+let nor = document.querySelector(".nor")
+nor.style.backgroundColor="pink"
+image.addEventListener("click",function(){
+    image.style.border="10px solid green"
+})
